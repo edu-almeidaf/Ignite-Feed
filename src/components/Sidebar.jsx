52bd5bcccec8d1b2b13a291ba4bf7c397d0c5cb0
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-05-25 23:57:56 -0300

    feat criando componente avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -10,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={ styles.profile }>
-        <img
-          src="https://github.com/edu-almeidaf.png"
-          className={ styles.avatar }  
-        />
+        <Avatar src="https://github.com/edu-almeidaf.png" />
         <strong>Eduardo de Almeida Fernandes</strong>
         <span>Web Developer</span>
       </div>
